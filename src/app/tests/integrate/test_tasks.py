ce84905868031d9ebac57adc4720aaa2bf21594d
fmirmuln <sabbirbeb@mail.ru>
2024-03-20 02:17:26 +0300

    feat: no token test

--- src/app/tests/integrate/test_tasks.py
+++ src/app/tests/integrate/test_tasks.py
@@ -7,7 +7,16 @@ class TestIntegrations:
 
         assert response.status_code == 200
 
-    def test_(self, test_client, unregisterd_user: User) -> None:  # noqa: ANN001
+    def test_no_bearer(self, test_client, unregisterd_user: User) -> None:  # noqa: ANN001
+        response = test_client.get(
+            "/api/v1/tasks",
+            follow_redirects=True,
+        )
+
+        assert response.status_code == 403
+
+
+    def test_no_authorization(self, test_client, unregisterd_user: User) -> None:  # noqa: ANN001
         response = test_client.get(
             "/api/v1/tasks",
             follow_redirects=True,
@@ -15,3 +24,4 @@ class TestIntegrations:
         )
 
         assert response.status_code == 403
+
