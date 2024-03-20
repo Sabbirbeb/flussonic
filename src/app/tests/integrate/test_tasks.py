82464fb36fdbd75674dd90c534bfeb8999e368fd
fmirmuln <sabbirbeb@mail.ru>
2024-03-20 23:47:06 +0300

    feat: gitignore updated

--- src/app/tests/integrate/test_tasks.py
+++ src/app/tests/integrate/test_tasks.py
@@ -31,10 +31,7 @@ class TestIntegrations:
 
         assert response.status_code == 403
 
-    async def test_create(self, client: AsyncClient, user: User, uow: UnitOfWork) -> None:  # noqa: ANN001
-        print (CreateTask(title="test_title",
-                            description="test_desc").model_dump_json(),)
-        
+    async def test_create(self, user: User, uow: UnitOfWork) -> None:  # noqa: ANN001
         async with httpx.AsyncClient() as session:
             response = await session.post(
                 "http://0.0.0.0:9000/api/v1/tasks",
@@ -44,15 +41,29 @@ class TestIntegrations:
                                 description="test_desc").model_dump_json()
             )
 
-            print (response.json())
-
         async with uow:
             result = await uow.tasks.get(response.json().get('id'))
         
-        print (result)
         assert response.status_code == 201
         assert result.title == "test_title"
         assert result.description == "test_desc"
     
-    
+    async def test_get(self, user: User, uow: UnitOfWork) -> None:  # noqa: ANN001
+        async with uow:
+            result = await uow.tasks.create(response.json().get('id'))
+        
+        print (result)
+        async with httpx.AsyncClient() as session:
+            response = await session.get(
+                "http://0.0.0.0:9000/api/v1/tasks",
+                follow_redirects=True,
+                headers={"Authorization": f"Bearer {user.name}"},
+            )
+
+            print (response.json())
+
+        
+        assert response.status_code == 200
+        assert result.title == "test_title"
+        assert result.description == "test_desc"
 
